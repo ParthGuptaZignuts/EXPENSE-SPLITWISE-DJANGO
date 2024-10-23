8eecf5327c204234c42e1e486eb5956ec0e1340d
ChitanZignuts <chintanr@zignuts.com>
2024-10-23 18:26:22 +0530

    pro testing

--- expense_splitwise_django_drf_backend/urls.py
+++ expense_splitwise_django_drf_backend/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    
 ]
 
 if 'core' in settings.INSTALLED_APPS:
